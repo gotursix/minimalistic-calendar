bc3ffdf1d7c3f2044c3b21b0ecbc8b418b56b044
Infernus <vly.2814@gmail.com>
2019-11-11 20:24:02 +0200

    Generate table

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-let today = newDate()
-let currentMonth = today.getMonth()
-let currentYear = today.getFullYear()
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
 
 let months = [
     'Jan',
@@ -15,13 +16,43 @@ let months = [
     'Oct',
     'Nov',
     'Dec'
-]
+];
 
-let monthAndYear = document.getElementById('monthAndYear')
+let monthAndYear = document.getElementById('monthAndYear');
+showCalendar(currentMonth, currentYear);
 
-function showCalendar() {
-    let firstDay = new Date(year, month).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+function showCalendar(month, year) {
+    let firstDay = new Date(year, month).getDay()
+    let daysInMonth = 32 - new Date(year, month, 32).getDate()
 
-    let tbl = document.getElementById('calendar-body');
+    let tbl = document.getElementById('calendar-body')
+
+    tbl.innerHTML = ' '
+
+    monthAndYear.innerHTML = months[month] + ' ' + year;
+
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement('tr');
+
+        for (let j = 0; j < 7; j++) {
+            if (i == 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode('');
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (date > daysInMonth) {
+                break;
+            } else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+
+            date++;
+        }
+
+        tbl.appendChild(row);
+    }
 }
